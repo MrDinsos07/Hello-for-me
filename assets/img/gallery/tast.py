3a4abc520b8e87e6648c9e5726dd4de06bd21e19
MrDinsos07 <87591930+MrDinsos07@users.noreply.github.com>
2023-09-27 12:24:38 +0700

    Dinsos07

---
+++ assets/img/gallery/tast.py
@@ -0,0 +1,32 @@
+from PIL import Image
+image = Image.open("gallery-1.jpg")
+new_size = (1920, 1440) 
+resized_image = image.resize(new_size)
+resized_image.save("gallery-1.jpg")
+
+image = Image.open("gallery-2.jpg")
+new_size = (1920, 1440) 
+resized_image = image.resize(new_size)
+resized_image.save("gallery-2.jpg")
+
+image = Image.open("gallery-3.jpg")
+new_size = (1920, 1440) 
+resized_image = image.resize(new_size)
+resized_image.save("gallery-3.jpg")
+
+image = Image.open("gallery-4.jpg")
+new_size = (1920, 1440) 
+resized_image = image.resize(new_size)
+resized_image.save("gallery-4.jpg")
+
+image = Image.open("gallery-5.jpg")
+new_size = (1920, 1440) 
+resized_image = image.resize(new_size)
+resized_image.save("gallery-5.jpg")
+
+image = Image.open("gallery-6.jpg")
+new_size = (1920, 1440) 
+resized_image = image.resize(new_size)
+resized_image.save("gallery-6.jpg")
+
+
